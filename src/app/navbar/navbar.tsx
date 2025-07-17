8b119d7b21d527d7f83d35f7ee3a053cf580ad82
mpinchover <mpinchover@gmail.com>
2025-07-16 23:18:31 -0700

    design done

---
+++ src/app/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Navbar() {
+  return (
+    <Flex
+      as="nav"
+      p={12}
+      bg="white"
+      boxShadow="0 1px 4px rgba(0, 0, 0, 0.2)"
+      align="center"
+      position="absolute"
+      width="100%"
+    >
+      <Text fontSize="2xl" fontWeight="bold" color="gray.700">
+        ReBreathe
+      </Text>
+    </Flex>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,65 @@
-import Image from "next/image";
+import { Box, Button, Flex, Text, VStack, Image } from "@chakra-ui/react";
+import doctor_image from "./image_1.png";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <Flex height="100vh" bg="white" align="center" justify="center" p={12}>
+      <Box mt={10} maxW={400}>
+        <Box fontSize="lg" color="gray.700" lineHeight={2}>
+          <Text as="p">Online virtual pulmonary rehab therapy.</Text>
+          <Text as="p">Customized sessions for each patient.</Text>
+          <Text as="p">Weekly analysis by board-certified pulmonologist.</Text>
+        </Box>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <VStack spacing={4} mt={6}>
+          <Button
+            p={6}
+            borderRadius="xl"
+            width="100%"
+            backgroundColor="black"
+            color="white"
+            // colorScheme="black"
+            variant="solid" // solid = filled background
+            fontWeight="semibold"
+            fontSize="md"
+            _hover={{}}
           >
+            Set up a session
+          </Button>
+          <Button
+            p={6}
+            borderRadius="xl"
+            width="100%"
+            // colorScheme="teal"
+            borderColor="grey"
+            variant="outline" // outline = bordered
+            fontWeight="semibold"
+            fontSize="md"
+            color="grey"
+            _hover={{ backgroundColor: "white" }}
+          >
+            Get in contact
+          </Button>
+        </VStack>
+
+        {/* Doctor Info Box */}
+        <Box mt={6} bg="gray.50" p={6} borderRadius="lg">
+          <Flex align="center" gap={6}>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={doctor_image.src}
+              alt="Dr. Zachary Pinchover"
+              height="120px"
+              width="120px"
+              borderRadius="full"
+              objectFit="cover"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <Text color="gray.700">
+              Dr. Zachary Pinchover is a Pulmonary and Critical Care Physician
+              specializing in Advanced Lung Diseases.
+            </Text>
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
