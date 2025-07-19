fefc24c54c177a152f8186cdc16b7f12f974ab01
mpinchover <mpinchover@gmail.com>
2025-07-19 13:15:16 -0700

    set up padding

--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -4,14 +4,14 @@ export default function Navbar() {
   return (
     <Flex
       as="nav"
-      p={{ base: 6, sm: 12 }}
+      p={4}
       bg="white"
       boxShadow="0 1px 4px rgba(0, 0, 0, 0.2)"
       align="center"
       position="absolute"
       width="100%"
     >
-      <Text fontSize="2xl" fontWeight="bold" color="gray.700">
+      <Text fontSize="xl" fontWeight="bold" color="gray.700">
         ReBreathe
       </Text>
     </Flex>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Box,
   Button,
@@ -6,77 +7,216 @@ import {
   VStack,
   Image,
   Link as CHLink,
+  Heading,
+  SimpleGrid,
 } from "@chakra-ui/react";
+import { Package, Video, Stethoscope } from "lucide-react"; // or your choice of icons
+
+import { useRouter } from "next/navigation";
+
 import doctor_image from "./image_1.png";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <Flex
-      height="100dvh"
+      minHeight="100dvh"
       bg="white"
       align={{ base: "start", sm: "center" }}
       justify="center"
-      p={{ base: 6, sm: 12 }}
+      flexDir={"column"}
+      // p={{ base: 6, sm: 12 }}
     >
-      <Box mt={{ base: 20, sm: 100 }} maxW={400}>
+      <Flex
+        mt={20}
+        width="100%"
+        bgColor="white"
+        // paddingY={50}
+
+        // p={{ base: 4, sm: 10 }}
+        paddingTop={{ base: 2, sm: 10 }}
+        paddingLeft={{ base: 2, sm: 5 }}
+        paddingRight={{ base: 2, sm: 5 }}
+        paddingBottom={{ base: 10, sm: 10 }}
+        alignItems="center"
+        justifyContent={"center"}
+        color="black"
+      >
         <VStack
-          // fontSize={12}
-          gap={2}
-          fontSize={{ base: "md", sm: "lg" }}
+          align="start"
+          width={{ base: 400, sm: 600 }}
+          maxW={{ base: 400, sm: 600 }}
+        >
+          <Heading size="3xl">Pulmonary rehabilitation from home</Heading>
+          <Box width="100%" mt={4}>
+            <Flex align="center" mb={2}>
+              <Package size={20} style={{ marginRight: 8 }} />
+              <Heading size="md">Equipment shipped to your home</Heading>
+            </Flex>
+
+            <Flex align="center" mb={2}>
+              <Video size={20} style={{ marginRight: 8 }} />
+              <Heading size="md">Virtual therapy appointments</Heading>
+            </Flex>
+
+            <Flex align="center">
+              <Stethoscope size={20} style={{ marginRight: 8 }} />
+              <Heading size="md">
+                Consultations with pulmonary specialists
+              </Heading>
+            </Flex>
+
+            <VStack mt={4} gapY={4}>
+              <Button
+                as="span"
+                p={{ base: 4, sm: 6 }}
+                borderRadius="xl"
+                width="100%"
+                backgroundColor="black"
+                color="white"
+                fontWeight="semibold"
+                fontSize={{ base: "sm", sm: "md" }}
+                variant="solid"
+                _hover={{}}
+                onClick={() => router.push("/signup")}
+              >
+                Set up a session
+              </Button>
+              <Button
+                p={{ base: 4, sm: 6 }}
+                borderRadius="xl"
+                width="100%"
+                borderColor="grey"
+                variant="outline"
+                fontWeight="semibold"
+                fontSize={{ base: "sm", sm: "md" }}
+                color="grey"
+                _hover={{ backgroundColor: "white" }}
+              >
+                Get in contact
+              </Button>
+            </VStack>
+          </Box>
+        </VStack>
+      </Flex>
+
+      <Flex
+        width="100%"
+        bgColor="gray.100"
+        paddingTop={{ base: 10, sm: 10 }}
+        paddingLeft={{ base: 2, sm: 5 }}
+        paddingRight={{ base: 2, sm: 5 }}
+        paddingBottom={{ base: 10, sm: 10 }}
+        alignItems="center"
+        justifyContent={"center"}
+      >
+        <VStack
+          // border="solid 1px green"
+          width={{ base: 400, sm: 600 }}
+          maxW={{ base: 400, sm: 600 }}
+          // border="solid 1px black"
+          // gapY={4}
+          fontSize={{ base: "sm", sm: "lg" }}
           color="gray.700"
           align="start"
         >
-          <Text as="p">Online virtual pulmonary rehab therapy.</Text>
-          <Text as="p">Customized sessions for each patient.</Text>
-          <Text as="p">Weekly analysis by board-certified pulmonologist.</Text>
-        </VStack>
+          <Heading>A typical session</Heading>
 
-        <VStack gap={4} mt={6}>
-          <CHLink
-            href="https://findhousing.typeform.com/to/IFtnsUS2"
-            target="_blank"
-            width="100%"
-            _hover={{ textDecoration: "none" }} // removes underline on hover
-          >
-            <Button
-              as="span"
-              p={{ base: 4, sm: 6 }}
-              borderRadius="xl"
-              width="100%"
-              backgroundColor="black"
-              color="white"
-              fontWeight="semibold"
-              fontSize={{ base: "sm", sm: "md" }}
-              variant="solid"
-              _hover={{}}
-            >
-              Set up a session
-            </Button>
-          </CHLink>
-
-          <Button
-            p={{ base: 4, sm: 6 }}
-            borderRadius="xl"
-            width="100%"
-            borderColor="grey"
-            variant="outline"
+          <SimpleGrid
+            // fontSize={"sm"}
             fontWeight="semibold"
-            fontSize={{ base: "sm", sm: "md" }}
-            color="grey"
-            _hover={{ backgroundColor: "white" }}
+            lineHeight={2}
+            columns={2}
+            w="100%"
           >
-            Get in contact
-          </Button>
+            <Box>
+              <Text>Warm-up</Text>
+              <Text>Aerobic training</Text>
+              <Text>Resistance training</Text>
+              <Text>Balance training</Text>
+              <Text>Cool down</Text>
+            </Box>
+            <Box textAlign="right">
+              <Text>5 min</Text>
+              <Text>25 min</Text>
+              <Text>25 min</Text>
+              <Text>10 min</Text>
+              <Text>5 min</Text>
+            </Box>
+          </SimpleGrid>
         </VStack>
+      </Flex>
 
-        {/* Doctor Info Box */}
-        <Box mt={6} bg="gray.50" p={6} borderRadius="lg">
+      <Flex
+        width="100%"
+        bgColor="white"
+        paddingTop={{ base: 10, sm: 10 }}
+        paddingLeft={{ base: 2, sm: 5 }}
+        paddingRight={{ base: 2, sm: 5 }}
+        paddingBottom={{ base: 10, sm: 10 }}
+        alignItems="center"
+        justifyContent={"center"}
+      >
+        <VStack
+          width={{ base: 400, sm: 600 }}
+          maxW={{ base: 400, sm: 600 }}
+          gap={4}
+          fontSize={{ base: "sm", sm: "lg" }}
+          color="gray.700"
+          align="start"
+        >
+          <Heading>12 session package</Heading>
+
+          <SimpleGrid
+            // fontSize={"sm"}
+            fontWeight="semibold"
+            lineHeight={2}
+            columns={2}
+            w="100%"
+          >
+            <Box>
+              <Text>Session 1</Text>
+              <Text>Sessions 2-5</Text>
+              <Text>Session 6</Text>
+              <Text>Session 7-11</Text>
+              <Text>Session 12</Text>
+            </Box>
+            <Box textAlign="right">
+              <Text>Intake</Text>
+              <Text>Pulm. Rehabilitation</Text>
+              <Text>Consultation</Text>
+              <Text>Pulm. Rehabilitation</Text>
+              <Text>Consultation</Text>
+            </Box>
+          </SimpleGrid>
+        </VStack>
+      </Flex>
+
+      {/* Doctor Info Box */}
+      <Flex
+        width="100%"
+        bgColor="gray.100"
+        paddingTop={{ base: 10, sm: 10 }}
+        paddingLeft={{ base: 2, sm: 5 }}
+        paddingRight={{ base: 2, sm: 5 }}
+        paddingBottom={{ base: 10, sm: 10 }}
+        alignItems="center"
+        justifyContent={"center"}
+      >
+        <VStack
+          width={{ base: 400, sm: 600 }}
+          maxW={{ base: 400, sm: 600 }}
+          gap={4}
+          fontSize={{ base: "sm", sm: "lg" }}
+          color="gray.700"
+          align="start"
+        >
           <Flex align="center" gap={6} flexDir={"column"}>
             <Image
               src={doctor_image.src}
               alt="Dr. Zachary Pinchover"
-              height={{ base: "80px", md: "120px" }}
-              width={{ base: "80px", md: "120px" }}
+              height={{ base: "80px", sm: "120px" }}
+              width={{ base: "80px", sm: "120px" }}
               borderRadius="full"
               objectFit="cover"
             />
@@ -88,14 +228,15 @@ export default function Home() {
                 fontWeight="semibold"
                 _hover={{ textDecoration: "underline" }}
               >
-                Dr. Zachary Pinchover
+                Dr Pinchover
               </CHLink>{" "}
-              is a Pulmonary and Critical Care Physician specializing in
-              Advanced Lung Diseases.
+              is a board certified pulmonologist who treats a wide range of
+              complex pulmonary diseases and is excited to give you the best
+              results and allow you to "ReBreathe" again.
             </Text>
           </Flex>
-        </Box>
-      </Box>
+        </VStack>
+      </Flex>
     </Flex>
   );
 }
