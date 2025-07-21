e177db83f23257eabaad11285586ab1de7cfbbd6
mpinchover <mpinchover@gmail.com>
2025-07-20 19:46:10 -0700

    use client on fb config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,4 @@
+"use client";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -20,4 +21,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
